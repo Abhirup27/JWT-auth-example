c5437bb1af9de9b7cb8638045c5bd6528c989195
Abhirup Bhattacharyya <55529237+Abhirup27@users.noreply.github.com>
2024-11-09 08:40:09 +0530

    access and refresh token working example

---
+++ authServer.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+const express = require('express');
+const app = express()
+const jwt = require("jsonwebtoken")
+
+app.use(express.json())
+
+let refreshTokens = [];
+
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        
+        if (err) return res.sendStatus(403);
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken: accessToken });
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token) //keeps the tokens in the array which do not match with req.body.token 
+    res.sendStatus(204)
+})
+app.post('/login', (req, res) => {
+   //auth user 
+
+    const username = req.body.username; // const passwd
+    const user = { name: username };
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+    res.json({ accessToken: accessToken,refreshToken: refreshToken });
+});
+
+
+// function authenticationToken(req, res, next)
+// {
+//     const authHeader = req.headers['authorization']
+//     const token = authHeader && authHeader.split(' ')[1];
+//     //BEARER token
+//     if (token == null) return res.sendStatus(401);
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        
+//         if (err) return res.sendStatus(403);
+//         req.user = user;
+//         next()
+//     })
+// }
+
+function generateAccessToken(user)
+{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'25s'});
+}
+app.listen(8081);
